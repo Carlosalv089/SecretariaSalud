515fd94513bae17c15625ca5f85e736a3f06767d
Carlos Alberto Alvarez Rodriguez <carlos.alvarez216543@potros.itson.edu.mx>
2022-02-13 23:56:50 -0700

    Prototipo proyecto Secretaria de Salud

---
+++ src/secretariasalud/SecretariaSalud.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secretariasalud;
+
+import vista.frmIniciarSesion;
+
+/**
+ *
+ * @author Carlos
+ */
+public class SecretariaSalud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmIniciarSesion frame = new frmIniciarSesion();
+        frame.setVisible(true);
+    }
+    
+}
